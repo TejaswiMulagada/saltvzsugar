db425d22f26e02bbe203bd9878236595ce7cccc3
tm053990 <tejaswi.mulagada@cerner.com>
2020-01-23 19:46:30 -0500

    Configure output files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
+const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './app.js',
     output: {
-        path: __dirname,
-        publicPath: '/',
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     watch: true,
     devServer: {
